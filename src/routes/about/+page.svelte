c7bafa9a48951cfeec46341fec6eda3ee2133536
Gareth Davies <gaz.andrew@gmail.com>
2025-06-21 17:43:49 +0100

    Update About page content and center text alignment

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,13 @@
-<p class="text-red-500 text-lg font-medium leading-relaxed m-5">
-  Welcome to the about page
+<script>
+
+</script>
+
+
+<p class="text-red-500 text-lg font-medium leading-relaxed m-5 text-center">
+  Welcome to the about page This is a test
+
 </p>
+
+
+
+
